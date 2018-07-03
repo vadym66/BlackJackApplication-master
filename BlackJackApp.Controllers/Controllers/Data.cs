fb9a98933693a134847dfa70b019ba882497b446
Anuitex-27 <Anuitex-27@DESKTOP-3M9Q6SI>
2018-07-03 10:34:16 +0300

    03jul

---
+++ BlackJackApp.Controllers/Controllers/Data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlackJackApp.Controllers.Controllers
+{
+    public class Data
+    {
+        public int OrderID { get; set; }
+        public int Freight { get; set; }
+        public string ShipName { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
