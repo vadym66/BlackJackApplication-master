2aa07472c432e8db72e6e82fb9aa78f1cc0cb254
Anuitex-27 <Anuitex-27@DESKTOP-3M9Q6SI>
2018-07-03 18:00:19 +0300

    03jul

--- BlackJackApp.AngularMvc/Properties/AssemblyInfo.cs
+++ BlackJackApp.AngularMvc/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BlackJackApp.Console.Test")]
+[assembly: AssemblyTitle("BlackJackApp.AngularMvc")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BlackJackApp.Console.Test")]
+[assembly: AssemblyProduct("BlackJackApp.AngularMvc")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9cbd222f-f65e-4d54-85a3-7d236a5ef5f5")]
+[assembly: Guid("56453ec8-9d1e-49aa-9792-c0cb8643ea67")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
